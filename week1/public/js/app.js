0bbb1ac5c0b2fb936926b67a6b82bc0b8d028c5a
roobinh <roobin1999@gmail.com>
2019-02-04 14:00:57 +0100

    added map

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -20,13 +20,31 @@ function addElements() {
 	paraLocation.appendChild(node);
 	div.appendChild(paraLocation);
 
+	var long = data['longitude'];
+	var lat = data['latitude'];
+
 	var paraLongLat = document.createElement("p");
 	var node = document.createTextNode("Lat/Lang: " + data['latitude'] + ", " + data["longitude"]);
 	paraLongLat.appendChild(node);	
 	div.appendChild(paraLongLat);
-		
+	
+	setMap(long, lat);	
 }
 
 xmlhttp.addEventListener('load', addElements);
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
+
+function setMap(long, lat) {
+	mapboxgl.accessToken = 'pk.eyJ1Ijoicm9vYmluMTk5OSIsImEiOiJjanJxYzVpeGIwdzJ4NDlycTZvd2lramRkIn0.jEoxjM-oE38jYCIHnhLw_g';
+	if (!mapboxgl.supported()) {
+	alert('Your browser does not support Mapbox GL');
+	} else {
+	var map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/mapbox/streets-v9',
+	center: [long, lat],
+	zoom: 12
+	});
+	}
+}
